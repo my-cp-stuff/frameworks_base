0a57ae3e4711c712e2c3afe9642edb592a3025b1
AshutoshSundresh <ashutoshsundresh@gmail.com>
2019-04-27 17:33:44 +0530

    base: Fix rgb accent picker for uimanager

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5017,6 +5017,12 @@ public final class Settings {
         public static final String COLOR_BUCKET_OVERLAY = "color_bucket_overlay";
 
         /**
+         * Accent Color
+         * @hide
+         */
+        public static final String ACCENT_COLOR = "accent_color";
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
@@ -5151,6 +5157,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(TORCH_LONG_PRESS_POWER_GESTURE);
             PRIVATE_SETTINGS.add(TORCH_LONG_PRESS_POWER_TIMEOUT);
             PRIVATE_SETTINGS.add(COLOR_BUCKET_OVERLAY);
+	        PRIVATE_SETTINGS.add(ACCENT_COLOR);
         }
 
         /**
